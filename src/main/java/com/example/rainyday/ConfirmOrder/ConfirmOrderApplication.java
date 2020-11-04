ee3ae5b4bb473e75ca21fae5ba598ec2eb2d5b66
akash.gupta8 <akash.gupta8@DEL1-LHP-N02569.synapse.com>
2020-11-04 13:25:20 +0530

    first commit

---
+++ src/main/java/com/example/rainyday/ConfirmOrder/ConfirmOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.rainyday.ConfirmOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfirmOrderApplication.class, args);
+
+}
+}
---
+++ src/test/java/com/example/rainyday/ConfirmOrder/ConfirmOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rainyday.ConfirmOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
